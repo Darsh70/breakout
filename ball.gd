22e6e72c7a30f7e5ef68acc854c7915fff8b7ec7
Darshan Prakash <52451615+Darsh70@users.noreply.github.com>
2025-11-12 08:52:12 -0600

    update ball direction based on paddle collision point

--- ball.gd
+++ ball.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
 signal point
+var speed = 300
 
 func _ready():
 	pass  
 
 func launch():
-	velocity = Vector2(200, 200)  
+	velocity = Vector2(1,1) * speed
 
 func launch_delayed(delay_time: float = 1.0):
 	# Stop the ball first
@@ -18,8 +19,29 @@ func launch_delayed(delay_time: float = 1.0):
 func _physics_process(delta: float) -> void:
 	var collision = move_and_collide(velocity * delta)
 	if collision:
-		velocity = velocity.bounce(collision.get_normal())
+		if collision.get_collider().name == "Player":
+
+			_bounce_from_paddle(collision)
+		else:
+			velocity = velocity.bounce(collision.get_normal())
 		
 		if collision.get_collider().is_in_group("blocks"):
+			speed += 100
+			velocity = velocity.normalized() * speed
 			collision.get_collider().destroy()
 			emit_signal("point")
+			
+func _bounce_from_paddle(collision):
+	var paddle = collision.get_collider()
+	var paddle_pos = paddle.global_position.x
+	var ball_pos = global_position.x
+	var paddle_shape = paddle.get_node("CollisionShape2D").shape
+	var paddle_width = paddle_shape.size.x
+	
+	var offset = (ball_pos - paddle_pos) / (paddle_width / 2)
+	
+	offset = clamp(offset, -1,1)
+	
+	var direction = Vector2(offset,-1).normalized()
+	
+	velocity = direction * speed
