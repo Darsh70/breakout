075e0f75f278fc6692fbd6a1ca17b5b5e25cc2f6
Darshan Prakash <52451615+Darsh70@users.noreply.github.com>
2025-11-12 08:52:31 -0600

    add save and high-score functionality

--- main.gd
+++ main.gd
@@ -2,11 +2,26 @@ extends Node2D
 
 var score = 0
 var lives = 3
+var highscore = 0
+const SAVE_PATH = "user://highscore.save"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	highscore = load_highscore()
 
+func save_highscore(value: int):
+	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
+	if file:
+		file.store_var(value)
+		file.close()
 
+func load_highscore():
+	if FileAccess.file_exists(SAVE_PATH):
+		var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
+		var value = file.get_var()
+		file.close()
+		return value
+	return 0
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	$Score/Label.text = str(score)
@@ -22,6 +37,10 @@ func _on_death_body_entered(body: Node2D) -> void:
 	if body.name == "Ball":
 		lives -= 1
 		if lives == 0:
+			if score > highscore:
+				highscore = score
+				save_highscore(highscore)
+			$Score/Label3.text = "Game Over" + "Score:" + str(score) + "HS:" + str(highscore)
 			$Score/Label3.visible = true
 			$Player.set_process(false)
 			$Score/Button.visible = true
@@ -30,6 +49,7 @@ func _on_death_body_entered(body: Node2D) -> void:
 	
 
 func restart():
+	$Ball.speed = 300
 	$Ball.position = $Player.position
 	$Ball.velocity = Vector2.ZERO 
 	$Ball.launch_delayed(1.0)
